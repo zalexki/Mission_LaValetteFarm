d07887553c9609786f8bbca5b04c5fa2d567197f
zalexki <haumalex@gmail.com>
2023-01-22 22:09:53 +0100

    cleanup and adding details in mission

--- Scripts/Game/UserActions/Modded/LiberateHostageUserAction.c
+++ Scripts/Game/UserActions/Modded/LiberateHostageUserAction.c
@@ -5,14 +5,15 @@ class LiberateHostageUserAction : ScriptedUserAction
 	override void PerformAction(IEntity pOwnerEntity, IEntity pUserEntity)
 	{
 		Print("start PerformAction");
-		canBeShown = false;
 		
 		AIAgent agent = AIAgent.Cast(GetGame().GetWorld().FindEntityByName("Hostage"));	
 		AIWaypoint waypoint = AIWaypoint.Cast(GetGame().GetWorld().FindEntityByName("Hostage_WP_GetInBTR_NoDriver"));
  		SCR_AIGroup groupAI = SCR_AIGroup.Cast(agent);
  		groupAI.AddWaypointToGroup(waypoint);
-	}
 
+		canBeShown = false;
+	}
+	
 	override bool CanBeShownScript(IEntity user) { return canBeShown; }
-	override bool HasLocalEffectOnlyScript() { return canBeShown; };
+	//override bool HasLocalEffectOnlyScript() { return false; };
 };
